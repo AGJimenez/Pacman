839edd0b6275041439d552845fda77e494560796
Alex Gámez <120292381+AGJimenez@users.noreply.github.com>
2024-01-24 00:40:10 +0100

    Fix

--- src/Personaje.java
+++ src/Personaje.java
@@ -2,40 +2,25 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public class Personaje implements Runnable {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public class Personaje {
     private int x;
     private int y;
     private int diametro;
     private Color color;
     private Rectangle[] limitesPaneles;
-    private boolean enMovimiento;
-    private int direccionX;
-    
+
     public Personaje(int x, int y, int diametro, Color color, Rectangle[] limitesPaneles) {
         this.x = x;
         this.y = y;
         this.diametro = diametro;
         this.color = color;
         this.limitesPaneles = limitesPaneles;
-        this.enMovimiento = true;
-        this.direccionX = 1;
-        while (colisionConLimites()) {
-            this.x = (int) (Math.random() * (498 - diametro));
-            this.y = (int) (Math.random() * (498 - diametro));
-        }
-        Thread hiloMovimiento = new Thread();
-        hiloMovimiento.start();
-    }
-    
-    public void setDireccionX(int direccionX) {
-        this.direccionX = direccionX;
     }
 
-    public void detenerMovimiento() {
-        enMovimiento = false;
-    }
-    
     public int getX() {
         return x;
     }
@@ -48,43 +33,21 @@ public class Personaje implements Runnable {
         return diametro;
     }
 
+    public void dibujar(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, diametro, diametro);
+    }
+
     public void mover(int deltaX, int deltaY) {
         x += deltaX;
         y += deltaY;
     }
 
-    public void dibujar(Graphics g) {
-    	 System.out.println("Dibujando personaje en: (" + x + ", " + y + ")");
-        g.setColor(color);
-        g.fillOval(x, y, diametro, diametro);
+    public void setX(int x) {
+        this.x = x;
     }
 
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, diametro, diametro);
+    public void setY(int y) {
+        this.y = y;
     }
-
-    public boolean colisionConLimites() {
-        for (Rectangle limitePanel : limitesPaneles) {
-            if (getBounds().intersects(limitePanel)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public void run() {
-        while (enMovimiento) {
-            mover(direccionX, 0);
-            // Agrega un pequeño retraso para controlar la velocidad del movimiento
-            try {
-                Thread.sleep(50); // Ajusta el valor según la velocidad deseada
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    
 }
-
--- src/Tablero.java
+++ src/Tablero.java
@@ -3,24 +3,60 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JSeparator;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JProgressBar;
 
+
+/* 
+ * 18/01/24: Vaya fumadón de práctica Daniel
+ * 19/01/24: Me quiero pegar un tiro
+ * 20/01/24: Actualización: he conseguido que el pacman se mueva
+ * 21/01/24: no sé ni lo que estoy haciendo
+ * 22/01/24: 4o dia, esto parece algo
+ * 23/01/24: Actualización: soy programador
+ * 
+ */
 public class Tablero extends JFrame implements KeyListener {
 
+	//Variables globales
+	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	 private JPanel tablero;
-	  private Personaje jugador;
-	   private Rectangle[] limitesPaneles;
-	   private int direccionX;
-	    private int direccionY;
+	private JPanel tablero;
+	private Personaje jugador; //creo el pacman
+	private Personaje fantasmaRojo, fantasmaNaranja, fantasmaAzul, fantasmaRosa; //declaro los fantasmicos
+	private Rectangle[] limitesPaneles; //creamos un array de la librería graphics para recorrer los obstáculos en el mapa
+	private int direccionX; //direccion hacia los lados. Predeterminada para el pacman hacia la derecha
+	private int vidas = 3;
+	private JLabel lbl_vida1, lbl_vida2, lbl_vida3;
+	Timer temporizador; //Clase Timer que me crea el temporizador
+	private int direccionY;
+	private Semaphore semaforoColision; //Con esto manejo colisiones entre fantasma y jugador
+	private AtomicBoolean colisionDetectada = new AtomicBoolean(false); //Tengo que usar "Atomic", que no lo he visto en mi vida, para que no pete
+	private  int tiempo_inicial = 120; // segundos
+	private int tiempoRestante;
+	private JProgressBar progressBar;
+	private JLabel lbl_temporizador;
+	private Thread hiloFantasmaRojo;
+	private Thread hiloFantasmaNaranja;
+	private Thread hiloFantasmaAzul;
+	private Thread hiloFantasmaRosa;
+	private Thread hiloMovimientoPacman;
+	private boolean pausaTemporal = false;
 	/**
 	 * Launch the application.
 	 */
@@ -38,6 +74,7 @@ public class Tablero extends JFrame implements KeyListener {
 	 * Create the frame.
 	 */
 	public Tablero() {
+
 		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        setBounds(100, 100, 805, 559);
 	        contentPane = new JPanel();
@@ -58,11 +95,73 @@ public class Tablero extends JFrame implements KeyListener {
 		panel.add(marcador);
 		marcador.setLayout(null);
 		
+		lbl_temporizador = new JLabel("New label");
+		lbl_temporizador.setBounds(70, 46, 60, 14);
+		marcador.add(lbl_temporizador);
+		lbl_temporizador.setText(String.valueOf(tiempoRestante));
+		
+		JLabel lblNewLabel = new JLabel("TIEMPO:");
+		lblNewLabel.setBounds(10, 21, 100, 14);
+		marcador.add(lblNewLabel);
+		
+		tiempoRestante = tiempo_inicial;
+		
+		progressBar = new JProgressBar();
+		progressBar.setString("");
+		progressBar.setBorderPainted(false);
+		progressBar.setBorder(null);
+		progressBar.setStringPainted(true);
+		progressBar.setBounds(10, 42, 182, 25);
+		marcador.add(progressBar);
+		
+		temporizador();
+
+		progressBar.setMaximum(tiempo_inicial); // Pone el valor máximo de la barra de progreso
+		progressBar.setValue(tiempo_inicial);
+		progressBar.setForeground(new Color(255, 128, 0)); 
+		
+		JLabel lbl_vidas = new JLabel("VIDAS:");
+		lbl_vidas.setBounds(10, 91, 46, 14);
+		marcador.add(lbl_vidas);
+		
+		JPanel panel_vidas = new JPanel();
+		panel_vidas.setBackground(Color.DARK_GRAY);
+		panel_vidas.setBounds(10, 116, 182, 48);
+		marcador.add(panel_vidas);
+		panel_vidas.setLayout(null);
+		
+		lbl_vida1 = new JLabel("");
+		lbl_vida1.setIcon(new ImageIcon(Tablero.class.getResource("/imagenes/pacman32.png")));
+		lbl_vida1.setBounds(20, 9, 32, 32);
+		panel_vidas.add(lbl_vida1);
+		
+		lbl_vida2 = new JLabel("");
+		lbl_vida2.setIcon(new ImageIcon(Tablero.class.getResource("/imagenes/pacman32.png")));
+		lbl_vida2.setBounds(75, 9, 32, 32);
+		panel_vidas.add(lbl_vida2);
+		
+		lbl_vida3 = new JLabel("");
+		lbl_vida3.setIcon(new ImageIcon(Tablero.class.getResource("/imagenes/pacman32.png")));
+		lbl_vida3.setBounds(129, 9, 32, 32);
+		panel_vidas.add(lbl_vida3);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				 temporizador.stop();
+			}
+		});
+		
 		  tablero = new JPanel() {
 	            @Override
 	            protected void paintComponent(Graphics g) {
 	                super.paintComponent(g);
 	                jugador.dibujar(g); // Usa el método dibujar de la clase Personaje
+	                fantasmaRojo.dibujar(g);
+	                fantasmaNaranja.dibujar(g);
+	                fantasmaRosa.dibujar(g);
+	                fantasmaAzul.dibujar(g);
+	                semaforoColision = new Semaphore(1); 
 	            }
 	        };
 	        tablero.setBackground(Color.DARK_GRAY);
@@ -72,7 +171,8 @@ public class Tablero extends JFrame implements KeyListener {
 	        tablero.setFocusable(true);
 	        tablero.addKeyListener(this);
 
-		
+		//Muchos paneles para crear el laberinto, esto después se recorrerá
+	        
 		JPanel panel_3_1_1_4 = new JPanel();
 		panel_3_1_1_4.setBounds(0, 395, 45, 16);
 		tablero.add(panel_3_1_1_4);
@@ -112,6 +212,26 @@ public class Tablero extends JFrame implements KeyListener {
 		JPanel panel_3_1_1_1_4 = new JPanel();
 		panel_3_1_1_1_4.setBounds(188, 297, 122, 16);
 		tablero.add(panel_3_1_1_1_4);
+		
+        JPanel panel_1 = new JPanel();
+        panel_1.setBounds(189, 260, 121, 10);
+        tablero.add(panel_1);
+        
+        JPanel panel_2 = new JPanel();
+        panel_2.setBounds(300, 219, 10, 51);
+        tablero.add(panel_2);
+        
+        JPanel panel_2_1 = new JPanel();
+        panel_2_1.setBounds(188, 219, 10, 51);
+        tablero.add(panel_2_1);
+        
+        JPanel panel_1_1 = new JPanel();
+        panel_1_1.setBounds(188, 209, 39, 10);
+        tablero.add(panel_1_1);
+        
+        JPanel panel_1_1_1 = new JPanel();
+        panel_1_1_1.setBounds(271, 209, 39, 10);
+        tablero.add(panel_1_1_1);
 		
 		JPanel panel_3_1_1_1_2 = new JPanel();
 		panel_3_1_1_1_2.setBounds(44, 347, 54, 16);
@@ -277,51 +397,86 @@ public class Tablero extends JFrame implements KeyListener {
 		parte_superior_exterior.setBounds(0, 0, 498, 10);
 		tablero.add(parte_superior_exterior);
 		
-	//	 inicializarLimitesPaneles();
-        // Agrega el KeyListener al JFrame para recibir eventos de teclado.
 		 
-		 // Obtiene todos los componentes (paneles) dentro de tablero
+		 // saca todos los componentes (paneles) dentro de tablero
 	        limitesPaneles = new Rectangle[tablero.getComponentCount()];
 	        for (int i = 0; i < tablero.getComponentCount(); i++) {
 	            limitesPaneles[i] = tablero.getComponent(i).getBounds();
 	        }
+	        //Inicializo los fantasmas
+	        fantasmaRosa = new Personaje(208, 232, 20, Color.PINK, limitesPaneles);
+	        fantasmaAzul = new Personaje(238, 232, 20, Color.CYAN, limitesPaneles);
+	        fantasmaNaranja = new Personaje(269, 232, 20, Color.ORANGE, limitesPaneles);
+	        fantasmaRojo = new Personaje(238, 207, 20, Color.RED, limitesPaneles);
 	        
-	        jugador = new Personaje(50, 50, 20, Color.YELLOW, limitesPaneles);
+	        //Inicializo el jugador
+	        jugador = new Personaje(238, 272, 20, Color.YELLOW, limitesPaneles);
 	        
 	        direccionX = 5;  // Dirección inicial hacia la derecha
 	        direccionY = 0;
 	        
-	        Thread hiloMovimiento = new Thread(() -> {
+	        //Usamos un hilo para mover al jugador y actualizar su posicion en el tablero con el método repaint
+	        hiloMovimientoPacman = new Thread(() -> {
 	            while (true) {
 	                moverJugador(direccionX, direccionY);
 	                tablero.repaint();
 
 	                try {
-	                    Thread.sleep(100);
+	                    Thread.sleep(100); //Cin esto puedo ajustar la velocidad del pacman
 	                } catch (InterruptedException e) {
 	                    e.printStackTrace();
 	                }
 	            }
 	        });
-	        hiloMovimiento.start();
-
+	        hiloMovimientoPacman.start(); //El hilo comienza a moverse
+	        
+	        
+	       //Inicializo los fantasmas como hiulos, llamando al método mover 
+	      hiloFantasmaRojo = new Thread(() -> moverFantasma(fantasmaRojo));
+	    hiloFantasmaNaranja = new Thread(() -> moverFantasma(fantasmaNaranja));
+	      hiloFantasmaRosa = new Thread(() -> moverFantasma(fantasmaRosa));
+	       hiloFantasmaAzul = new Thread(() -> moverFantasma(fantasmaAzul));
+	        
+	        
+	        hiloFantasmaRojo.start();
+	        hiloFantasmaNaranja.start();
+	        hiloFantasmaRosa.start();
+	        hiloFantasmaAzul.start();
+	        
 	        addKeyListener(this);
 	        tablero.setFocusable(true);
+	        
+
 	        tablero.requestFocusInWindow();
 	    
 	}
 	
 	
-    // Inicializa los límites de los paneles dentro del tablero
-    private void inicializarLimitesPaneles() {
-        // Ajusta estos valores según las posiciones y tamaños reales de tus paneles
-        limitesPaneles = new Rectangle[] {
-                new Rectangle(0, 250, 98, 10),
-                new Rectangle(400, 250, 98, 10),
-                // Agrega más rectángulos según sea necesario
-        };
-    }
+	private void temporizador() {
+		temporizador = new Timer(1000, e -> {
+		    if (!pausaTemporal) {
+		        tiempoRestante--;
 
+		        // Actualiza la barra de progreso con el tiempo restante
+		        progressBar.setValue(tiempoRestante);
+
+		        // Actualiza la etiqueta con el tiempo restante, uso las utilidadess de swing porque si no, no muestra la etiqueta bien
+		        SwingUtilities.invokeLater(() -> lbl_temporizador.setText(String.valueOf(tiempoRestante)));
+
+		        // mria si el tiempo ha llegado a cero
+		        if (tiempoRestante <= 0) {
+		            
+		            ((Timer) e.getSource()).stop();
+		            terminarJuego();
+		            colisionDetectada.set(false);
+		        }
+		    }
+		});
+
+
+		// Inicia el temporizador
+		temporizador.start();
+	}
 	
     // Método para verificar si la nueva posición del jugador está dentro de los límites del tablero y sin colisiones con los paneles
     private boolean esPosicionValida(int x, int y) {
@@ -347,12 +502,163 @@ public class Tablero extends JFrame implements KeyListener {
         int nuevaPosY = jugador.getY() + deltaY;
 
         if (esPosicionValida(nuevaPosX, nuevaPosY)) {
-        	 System.out.println("Posición del jugador después de mover: (" + jugador.getX() + ", " + jugador.getY() + ")");
+        	// System.out.println("Posición del jugador después de mover: (" + jugador.getX() + ", " + jugador.getY() + ")");
             jugador.mover(deltaX, deltaY);
-            tablero.repaint();  // Asegúrate de llamar a repaint después de mover al jugador
+            tablero.repaint();  // 
+            verificarColisiones(); // Después de mover al player, verifico la colision
         }
     }
 
+    
+    private void verificarColisiones() {
+        Rectangle jugadorRect = new Rectangle(jugador.getX(), jugador.getY(), jugador.getDiametro(), jugador.getDiametro());
+
+        // Verifica colisiones con los fantasmas
+        Personaje[] fantasmas = {fantasmaRojo, fantasmaNaranja, fantasmaRosa, fantasmaAzul};
+        for (Personaje fantasma : fantasmas) {
+            Rectangle fantasmaRect = new Rectangle(fantasma.getX(), fantasma.getY(), fantasma.getDiametro(), fantasma.getDiametro());
+            if (jugadorRect.intersects(fantasmaRect)) {
+                // Colisión detectada
+            	
+                mostrarDialogoColision();
+                return;  // Salgo del bucle después de la primera colisiópn encontrada
+            }
+        }
+    }
+    
+    private void restarVida() {
+        vidas--;
+
+        // Actualizaaz la representacióin de los iconitos
+        switch (vidas) {
+            case 2:
+                lbl_vida3.setVisible(false);
+                break;
+            case 1:
+                lbl_vida2.setVisible(false);
+                break;
+            case 0:
+                lbl_vida1.setVisible(false);
+                terminarJuego();
+                break;
+        }
+    }
+
+
+    private void mostrarDialogoColision() {
+        if (!colisionDetectada.getAndSet(true)) {
+        	pausaTemporal = true;
+            // Muestra un joption para la colision
+            try {
+                semaforoColision.acquire(); // cojo el semáforo para evitar colisiones concurrentes (wtf)
+                JOptionPane.showMessageDialog(this, "Ñam,ñam, qué rico... Tienes una vida menos");
+                restarVida(); //restamos vida
+                // Reinicia las posiciones del jugador y los fantasmas
+                reiniciarJuego();
+                colisionDetectada.set(false);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaforoColision.release(); // Libera el semáforo para permitir otras colisiones
+                pausaTemporal = false;
+            }
+        }
+    }
+
+
+    private void reiniciarJuego() {
+        // Reinicia las posiciones del jugador y los fantasmas
+        jugador.setX(238);
+        jugador.setY(272);
+
+        fantasmaRojo.setX(208);
+        fantasmaRojo.setY(232);
+
+        fantasmaAzul.setX(238);
+        fantasmaAzul.setY(232);
+
+        fantasmaNaranja.setX(269);
+        fantasmaNaranja.setY(232);
+
+        fantasmaRosa.setX(238);
+        fantasmaRosa.setY(207);
+
+        // Repinta el tablero para reflejar los cambios
+        tablero.repaint();
+    }
+
+    
+    private void moverFantasma(Personaje fantasma) {
+        Random random = new Random();
+        int duracionMovimiento = 500; // Duración total del movimiento en milisegundos
+        int cantidadPasos = 20; // Número de pasos para cada movimiento
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                int pasoX = (random.nextInt(3) - 1) * 2; // Genera -2, 0, o 2
+                int pasoY = (random.nextInt(3) - 1) * 2; // Genera -2, 0, o 2
+
+                for (int i = 0; i < cantidadPasos; i++) {
+                    int nuevaPosX = fantasma.getX() + pasoX;
+                    int nuevaPosY = fantasma.getY() + pasoY;
+
+                    // Verifica colisiones con otros fantasmas y actualiza la posición
+                    Rectangle nuevoRect = new Rectangle(nuevaPosX, nuevaPosY, fantasma.getDiametro(), fantasma.getDiametro());
+                    boolean colision = false;
+
+                    for (Personaje otroFantasma : new Personaje[]{fantasmaRojo, fantasmaNaranja, fantasmaRosa, fantasmaAzul}) {
+                        if (otroFantasma != fantasma) {
+                            Rectangle otroRect = new Rectangle(otroFantasma.getX(), otroFantasma.getY(), otroFantasma.getDiametro(), otroFantasma.getDiametro());
+                            if (nuevoRect.intersects(otroRect)) {
+                                // Hay colisión con otro fantasma, evita la colisión
+                                colision = true;
+                                break; // Sal del bucle interno
+                            }
+                        }
+                    }
+
+                    if (!colision && esPosicionValida(nuevaPosX, nuevaPosY)) {
+                        fantasma.mover(pasoX, pasoY);
+                        tablero.repaint();
+                        verificarColisiones();
+
+                        // Espera antes de realizar el siguiente paso
+                        Thread.sleep(duracionMovimiento / cantidadPasos);
+                    } else {
+                        // Cambia la dirección para evitar la colisión
+                        pasoX = (random.nextInt(3) - 1) * 2;
+                        pasoY = (random.nextInt(3) - 1) * 2;
+                    }
+                }
+            } catch (InterruptedException e) {
+                // Maneja la interrupción saliendo del bucle
+                break;
+            }
+        }
+    } //MENCION HONORIFICA A CHATTY POR HACERME LOS RANDOM, SÍ, NO SÉ DE MATEMÁTICAS.
+
+
+    private void terminarJuego() {
+      
+            temporizador.stop();
+        
+
+        //Me cargo los hilos y next
+        hiloMovimientoPacman.interrupt();
+        hiloFantasmaAzul.interrupt();
+        hiloFantasmaNaranja.interrupt();
+        hiloFantasmaRojo.interrupt();
+        hiloFantasmaRosa.interrupt();
+        
+   
+       //INTENTAR FINALIZAR HILO DE JUGADOR, QUE NO LO CONSIGO
+
+        JOptionPane.showMessageDialog(this, "GAME OVER");
+        dispose();
+        PantallaFinal finJuego = new PantallaFinal();
+        finJuego.setVisible(true);
+    }
+    
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
